8a9c2864e7894b7cb092c3ed4c1e46a95738f4c5
luojunbin <luojunbin@baidu.com>
2016-10-04 13:30:40 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # node-client-upload
-Use nodejs as the client for file upload
+一个以 node 为客户端进行文件上传的工具; 适用于一些远端机器 ftp 服务受限的场景;
+
+## receiver
+本模块只包含发送端的代码, 接收端的代码使用了 [fex](http://fex.baidu.com/) 开发的工具---[receiver.php](https://github.com/fex-team/fis-command-release/blob/master/tools/receiver.php) 以及 node 版本的 [receiver.js](https://github.com/fex-team/receiver)
+
+## API
+
+upload(receiverUrl, filePath, formData, callback);
+
+第一个参数是 receiver 的地址;
+第二个参数是要上传的文件的本地路径;
+第三个参数是表单数据, 为了与 receiver 配合, 一般需要填写一个 `to` 属性指定上传到哪个远端的路径下;
+第四个参数是回调函数;
+
+示例如下:
+
+```
+upload('http://10.10.10.10:8765/receiver.php', 'test.png', {
+    to: '/home/users/username/www/static/test.png'
+}, function (res, body) {
+    console.log(res, body);
+});
+```
---
+++ src/upload.js
@@ -0,0 +1,72 @@
+let fs = require('fs');
+let Url = require('url');
+let path = require('path');
+
+const dash = '--';
+const crlf = '\r\n';
+
+function item(key, value, boundary) {
+    return [
+        `${dash}${boundary}${crlf}`,
+        `Content-Disposition: form-data; name="${key}"${crlf}`,
+        crlf,
+        value,
+        crlf
+    ];
+}
+
+function getOptions(url, headers) {
+    let opt = Url.parse(url);
+
+    return {
+        headers,
+        agent: false,
+        method: 'POST',
+        port: opt.port,
+        path: opt.path,
+        host: opt.hostname,
+        protocol: opt.protocol
+    };
+}
+
+function getFormData(data, filePath, boundary) {
+    let content = fs.readFileSync(path.join(__dirname, filePath));
+
+    let dataFormate = Object.keys(data).reduce((prev, key) => [...prev, ...item(key, data[key], boundary)], []);
+
+    return [
+        ...dataFormate,
+        ...item(`file"; filename="${path.basename(filePath)}`, content, boundary),
+        `${dash}${boundary}${dash}${crlf}`
+    ];
+}
+
+function upload(url, filePath, data, callback) {
+
+    const boundary = `-----np${Math.random()}`;
+
+    let formData = getFormData(data, filePath, boundary);
+    let length = formData.reduce((prev, next) => {
+        return typeof next === 'string'
+            ? prev += new Buffer(next).length
+            : prev += next.length;
+    }, 0);
+
+    let opt = getOptions(url, {
+        'Content-Type': `multipart/form-data; boundary=${boundary}`,
+        'Content-Length': length
+    });
+
+    let client = opt.protocol === 'https:' ? require('https') : require('http');
+
+    let req = client.request(opt, res => {
+        let body = '';
+
+        res.on('data', chunk => body += chunk);
+        res.on('end', () => callback(res, body));
+    });
+
+    req.on('error', e => console.log(`problem with request: ${e.message}`));
+    formData.forEach(v => req.write(v));
+    req.end();
+}
